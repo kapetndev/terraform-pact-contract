cd0a831bed08b7327007e777568cf94a17ab7353
Tomas Basham <tomasbasham@gmail.com>
2023-08-08 22:18:31 +0100

    docs(contract): add example usage

---
+++ examples/consumer-provider-pact/main.tf
@@ -0,0 +1,15 @@
+module "my_contract" {
+  source = "git::https://github.com/kapetndev/terraform-pact-contract.git?ref=v0.1.0"
+
+  consumer = {
+    name       = "my-consumer"
+    repository = "https://github.com/kapetndev/my-consumer"
+  }
+
+  providers = [
+    {
+      name       = "my-provider"
+      repository = "https://github.com/kapetndev/my-provider"
+    },
+  ]
+}
